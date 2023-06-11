48589c53244ea4128a53dd75d90af53f83c42a14
Marc Eichler <marc@fam-eichler.de>
2023-06-11 09:06:01 +0200

    Fixed backup script

--- roles/container-backup/files/backup-container.sh
+++ roles/container-backup/files/backup-container.sh
@@ -6,6 +6,9 @@ container=$1
 backup_dir=/var/backup/$container
 data_dir=$2
 
+keep_short_max_age=7
+keep_long_max_age=45
+
 backup_file="$container-$(date +%F-%H%M%S).tar.lz4"
 
 echo "### Executing backup for $container"
@@ -18,4 +21,13 @@ docker run --rm --volumes-from "$container" -v "$backup_dir":/backup --name "$co
 echo "Starting $container container..."
 docker start "$container"
 
-echo "Done"
+echo "Deleting backups older than $keep_short_max_age days..."
+# Delete backups older than 7 days, unless they were created on monday
+LC_TIME=C find "$backup_dir" -type f -mtime +$keep_short_max_age -exec sh -c 'test $(date +%a -r "$1") = Mon || rm "$1"' -- {} \;
+
+# Delete all backups older than 45 days
+echo "Deleting backups older than $keep_long_max_age days..."
+find "$backup_dir" -type f -mtime +$keep_long_max_age -exec rm {}\;
+
+echo "Done."
+exit 0
